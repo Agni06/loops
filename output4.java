d0bcc2af131632dbaf0d581072c6d718ea1be4da
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-02-15 17:58:10 +0530

    Create output4.java

---
+++ output4.java
@@ -0,0 +1,10 @@
+public class Demo {
+	public static void main(String[] args) {
+		int counter = 1;
+		while (counter < 3) {
+			counter = counter + 1;
+			System.out.println(counter);
+		}
+	}
+}
+ 
