b5abefc5fcb10ab26c0617b040d2cc0f1415f066
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-02-15 18:03:08 +0530

    Create output6.java

---
+++ output6.java
@@ -0,0 +1,14 @@
+public class Demo {
+	public static void main(String[] args) {
+		int num1;
+		int num2;
+		while (num1 >= 2) {
+			if (num1 > num2) {
+				num1 = num1 / 2;
+			} else {
+				System.out.println(num1);
+				break;
+			}
+		}
+	}
+}
